f231fb1e530aa846c95fc3e0cff9ac912ad0e3a3
ri10@ <aristic665@gmail.com>
2022-03-30 16:50:39 +0200

    initiat commit

---
+++ src/pages/Finish.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Finish = () => {
+	return <div>F</div>
+}
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { Start } from './Start'
+export { Review } from './Review'
+export { Scoreboard } from './Scoreboard'
+export { Question } from './Question'
+export { Form } from './Form'
+export { Finish } from './Finish'
